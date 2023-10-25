c940059ab8f2b0a839844532ffcda0440501cfe9
Quân Phạm Ngọc <ngocquanofficial@gmail.com>
2023-10-25 15:31:22 +0700

    initial commit

---
+++ model.py
@@ -0,0 +1,9 @@
+import torch 
+import torch.nn as nn
+import math
+class Model(nn.Module) :
+    def __init__(self, model_config) :
+        super().__init__()
+        
+    def forward(self) :
+        return None
---
+++ test_code.py
@@ -0,0 +1,7 @@
+my_dict = {"a": 1, "b": 2, "c": 3}
+
+# Get the length of the dictionary
+dict_length = len(my_dict)
+
+# Print the length
+print("Length of the dictionary:", dict_length)
