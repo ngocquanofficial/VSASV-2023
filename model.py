49b5d7be22d9730114f80655387eb7ba62165505
Quân Phạm Ngọc <ngocquanofficial@gmail.com>
2023-10-29 10:36:42 +0700

    update

--- model.py
+++ model.py
@@ -43,6 +43,7 @@ class TripletLoss(nn.Module):
     def forward(self, anchor, positive, negative, size_average=True):
         distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
         distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
+        print(losses)
         print(f"Positive: {distance_positive}, Negative: {distance_negative}")
         losses = F.relu(distance_positive - distance_negative + self.margin)
         return losses.mean() if size_average else losses.sum()
